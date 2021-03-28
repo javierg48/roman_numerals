47ef244ba70d637f6983fc203d8db4575f469f17
android-47 <jx.garcia@outlook.com>
2021-03-27 20:51:40 -0700

    Removed stringToInteger function and added a traverse function. Replaced the switch statement with an if else statement. Removed global variables and updated arrays. Updated README too.

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# roman_numerals
+# Roman Numeral Calculator
+
+### FEATURES
+* Accurately converts an unsigned integer to it's Roman numeral value
+* Unsigned integer can range from 1 and 1 billion, inclusively
+
+## GAMEPLAY
+![test1](./images/test1.png "")
+![test2](./images/test2.png "")
+![test3](./images/test3.png "")
+
+
+### DESCRIPTION
+This program acts as a calculator. The input for this program is an unsigned integer between 1 and 1 billion (inclusive) from the user and outputs the number as a Roman numeral. 
+
+### ROMAN NUMERAL KEY
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1,000
+
+Note: There is ONE horizontal lines above each Roman numeral
+V̅ = 5,000
+X̅ = 10,000
+L̅ = 50,000
+C̅ = 100,000
+D̅ = 500,00
+M̅ = 1,000,000
+
+Note: There are TWO horizontal lines above each Roman numeral
+V̿ = 5,000,000
+X̿ = 10,000,000
+L̿ = 50,000,000
+C̿ = 100,000,000
+D̿ = 500,000,000
+M̿ = 1,000,000,000
+
+### STEPS ON HOW TO COMPILE AND RUN THE PROGRAM
+1. Compile the program using C++ 11 by entering the following into the terminal command line  
+
+```
+g++ -std=c++11 roman_numerals.cpp
+```
+
+2. Run the program by running the executable file by entering the following line into the terminal command line:
+
+```
+./a.out
+```
+
+### KNOWN LIMITATIONS
+* No error handling
+* For numbers 5,000,000 and above, the two horizontal lines above the Roman numeral are difficult to see
--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,96 +1,58 @@
 // author: Javier Garcia Ramirez
-// create date: Sunday, January 12th, 2020
-// last modified: Sunday, September 20, 2020
+// create date: Sunday, January 12, 2020
+// last modified: Saturday, March 27, 2021
 // filename: roman_numerals.cpp
 // description: receives input from user then outputs the roman numeral value of the input
 // runtime: n/a
 
 #include <iostream>
-#include <cstring>
-using namespace std;
-
-int globalStringLength, adjust = 0;
+#include <string>
 
 
-string userStringNumber; // Input by user will be received as string.
-// Declare values for numbers leading/starting with the number 1.
-string one = "I", ten = "X", hundred = "C", thousand = "M", tenThousand = "X̅";
-// Declare values for numbers leading/starting with the number 5.
-string five = "V", fifty = "L", fiveHundred = "D", fiveThousand = "V̅", fiftyThousand = "L̅"
-, fiveHundredThousand = "D̅", fiveMillion = "V̿", fiftyMillion = "L̿", fiveHundredMillion = "D̿";
-// Declare values for numbers leading/starting with the number 1.
-string oneHundredThousand = "C̅", oneMillion = "M̅", tenMillion = "X̿", hundredMillion = "C̿", oneBillion = "M̿";
+void printRomanNumeral(std::string userInput, int inputLength);
+void traverse(std::string *array, int inputLength, int currentNum, int x);
 
 
-int globalNumberSplit[100]; // 10 is the most amount of digits the user's input can have.
+int main() {
+    // receive input as string because it makes it easier to get it's length
+    std::string userInput;
+    std::cout << "\nEnter a number between 1 and 1 billion: "; // should be less than 10 digits
+    std::cin >> userInput;
 
-string leadingOneList[10] = {one, ten, hundred, thousand, tenThousand, 
-oneHundredThousand, oneMillion, tenMillion, hundredMillion, oneBillion};
+    int inputLength = userInput.length(); 
+    int userNum = stoi(userInput); // convert userInput string to integer
 
-string leadingFiveList[9] = {five, fifty, fiveHundred, fiveThousand, 
-fiftyThousand, fiveHundredThousand, fiveMillion, fiftyMillion, fiveHundredMillion};
-
-
-void stringToInteger();
-void printRomanNumeral();
-
-
-int main() {   
-    cout << "\nEnter a number between 1 and 1 billion: ";
-    cin >> userStringNumber;
-    globalStringLength = userStringNumber.length();
-
-    stringToInteger();
-    cout << userStringNumber << " as a Roman Numeral: ";
-
-    printRomanNumeral();
-    cout << endl << endl;
+    std::cout << userNum << " as a Roman Numeral: ";
+    printRomanNumeral(userInput, inputLength);
+    std::cout << std::endl << std::endl;
     return 0;
 }
 
 
-void stringToInteger() {
- char charArray[globalStringLength + 1];
-   int intNumber;
-   for (unsigned i= 0; i < globalStringLength; i++) {
-        strcpy(charArray, userStringNumber.c_str());   // copy string userStringNumber into charArray
-        intNumber = charArray[i] - '0'; // convert char  value to int value by subracting ascii value of charArray[i] from the ascii value of 0 to get int value
-        globalNumberSplit[i] = intNumber;         // save the int value into the int array
-   }
+void printRomanNumeral(std::string userInput, int inputLength) {
+    std::string onesList[] = { "I", "X", "C", "M", "X̅",  "C̅", "M̅", "X̿", "C̿", "M̿" };
+    std::string fivesList[] = {"V", "L", "D", "V̅", "L̅", "D̅", "V̿", "L̿", "D̿"};
+
+    int currentNum = 0;
+    for(int i = 0; 0 <= inputLength; i++) {
+        currentNum = userInput[i] - '0'; // convert character within string of userInput to an int
+        inputLength -= 1;
+
+        if (1 <= currentNum && currentNum <= 3) { // num between 1 and 3 (inclusive)
+            traverse(onesList, inputLength, currentNum, 0);
+        } else if (currentNum == 4) {
+            std::cout << onesList[inputLength] + fivesList[inputLength];
+        } else if (5 <= currentNum && currentNum <= 8) { // num between 5 and 8 (inclusive)
+            std::cout << fivesList[inputLength];
+            traverse(onesList, inputLength, currentNum, 5);
+        } else if (currentNum == 9) {
+            std::cout << onesList[inputLength] + onesList[inputLength+1];        
+        }
+    }
 }
 
-void printRomanNumeral() {
-    int placeholder;
-    int index = 0;
-    while (0 <= globalStringLength) {
-        placeholder = globalNumberSplit[index]; // adjust makes sure the index is going through each element in globalNumberSplit
-            // prints digit for numbers equal to or smaller than 3.
-        switch(placeholder) {
-            case 1: case 2: case 3: 
-                for(int x = 0; x < placeholder ;x++) {
-                    cout << leadingOneList[globalStringLength-1];
-                }
-                break;
-                // prints if globalNumberSplit[adjust] is 4
-            case 4: 
-                cout << leadingOneList[globalStringLength-1] + leadingFiveList[globalStringLength-1];
-                break;
-                // prints 5 value automatically if number is equal to or greater than 5 but less than 9. Then prints digit for each number over 5
-            case 5: case 6: case 7: case 8:
-                cout << leadingFiveList[globalStringLength-1];
-                // x = 5 because if globalNumberSplit[adjust] = 5, it will not run the for loop which means it will not print unneeded digits
-                for(int x = 5; x < placeholder ;x++){
-                    cout << leadingOneList[globalStringLength-1];
-                    }
-                break;
-                // prints if globalNumberSplit[adjust] is 9
-            case 9:
-                cout << leadingOneList[globalStringLength-1] + leadingOneList[globalStringLength];
-                break;
-	}
-        index = index + 1;        // in globalNumberSplit, it moves the index to the element on the right 
-        // keeps s the same value of 0 because if we increase s and keep globalStringLength, then printint from leadingOneList or leadingFiveList becomes more difficult.
-        globalStringLength = globalStringLength -1;   // since s is staying the same, we are decreasing globalStringLength to eventually end the for loop
-    }
 
+void traverse(std::string *array, int inputLength, int currentNum, int x) {
+    for(int i = x; i < currentNum; i++) // x will be either 0 or 5
+        std::cout << array[inputLength];
 }
